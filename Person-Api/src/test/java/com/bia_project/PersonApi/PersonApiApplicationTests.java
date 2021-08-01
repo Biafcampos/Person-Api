5e0002f247459c43be0ab1d8ddc0330cba54ca0f
Bianca <66906291+Biafcampos@users.noreply.github.com>
2021-07-31 21:30:54 -0300

    update files

---
+++ Person-Api/src/test/java/com/bia_project/PersonApi/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bia_project.PersonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
